f7df4dc7b82bd725f872d265053d987bbc68de2d
itadigital <csaldanha@itagroup.com>
2020-11-16 15:38:56 -0600

    Add files via upload

--- ita-comment-push.php
+++ ita-comment-push.php
@@ -1,16 +1,16 @@
 <?php
-$servername = "ec2-34-237-166-54.compute-1.amazonaws.com";
-$username = "gyuewavwrbddfh";
-$password = "70345c071e84599006ac8375ca65adc894ccafd5c13cf2b2f1ef770bf061d610";
-$dbname = "d46lbrs575uafs";
+   $host        = "host=ec2-34-237-166-54.compute-1.amazonaws.com";
+   $port        = "port=5432";
+   $dbname      = "dbname=d46lbrs575uafs";
+   $credentials = "user=gyuewavwrbddfh password=70345c071e84599006ac8375ca65adc894ccafd5c13cf2b2f1ef770bf061d610";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 
 
-$conn->close();
+   pg_close($db);
 ?>
