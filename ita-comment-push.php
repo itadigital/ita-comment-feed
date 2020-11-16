6e04e4c2f8fc8794c0d72821edb502f89fda5950
itadigital <csaldanha@itagroup.com>
2020-11-16 15:48:13 -0600

    Add files via upload

--- ita-comment-push.php
+++ ita-comment-push.php
@@ -1,16 +1,41 @@
 <?php
-   $host        = "host=ec2-34-237-166-54.compute-1.amazonaws.com";
-   $port        = "port=5432";
-   $dbname      = "dbname=d46lbrs575uafs";
+   $host = "host=ec2-34-237-166-54.compute-1.amazonaws.com";
+   $port = "port=5432";
+   $dbname = "dbname=d46lbrs575uafs";
    $credentials = "user=gyuewavwrbddfh password=70345c071e84599006ac8375ca65adc894ccafd5c13cf2b2f1ef770bf061d610";
 
-   $db = pg_connect( "$host $port $dbname $credentials"  );
+   $db = pg_connect( "$host $port $dbname $credentials" );
    if(!$db) {
-      echo "Error : Unable to open database\n";
+   echo "Error : Unable to open database\n";
    } else {
-      echo "Opened database successfully\n";
+   echo "Opened database successfully\n";
    }
 
+   // id, eventName, firstName, lastName, fullName, location, comment, timeStamp, imageUpload
 
-   pg_close($db);
+   $date = new DateTime();
+   $ts = $date->getTimestamp();
+
+   $sql =<<<EOF 
+       INSERT INTO commentFeed (eventName, comment, timeStamp) 
+       VALUES ('2I79C1', 'this is a test comment' , $ts );
+    EOF;
+
+    $ret = pg_query($db, $sql);
+
+    if(!$ret) {
+      echo pg_last_error($db);
+      exit;
+    } 
+   
+    while($row = pg_fetch_row($ret)) {
+      echo "id = ". $row[8] . "\n";
+      echo "eventName = ". $row[0] ."\n";
+      echo "comment = ". $row[5] ."\n";
+      echo "timeStamp =  ".$row[6] ."\n\n";
+    }
+   
+    echo "Operation done successfully\n";
+
+    pg_close($db);
 ?>
