f76bdf8ceb4a803cb0030437699d5f8bb4250dc2
itadigital <csaldanha@itagroup.com>
2020-11-16 23:10:23 -0600

    Add files via upload

--- ita-comment-push.php
+++ ita-comment-push.php
@@ -1,30 +1,33 @@
 <?php
-   $host = "host=ec2-34-237-166-54.compute-1.amazonaws.com";
-   $port = "port=5432";
-   $dbname = "dbname=d46lbrs575uafs";
-   $credentials = "user=gyuewavwrbddfh password=70345c071e84599006ac8375ca65adc894ccafd5c13cf2b2f1ef770bf061d610";
+    date_default_timezone_set('America/Chicago');
 
-   $db = pg_connect( "$host $port $dbname $credentials" );
-   if(!$db) {
-   echo "Error : Unable to open database\n";
-   } else {
-   echo "Opened database successfully\n";
-   }
+    $host = "host=ec2-34-237-166-54.compute-1.amazonaws.com";
+    $port = "port=5432";
+    $dbname = "dbname=d46lbrs575uafs";
+    $credentials = "user=gyuewavwrbddfh password=70345c071e84599006ac8375ca65adc894ccafd5c13cf2b2f1ef770bf061d610";
 
-   // id, eventName, firstName, lastName, fullName, location, comment, timeStamp, imageUpload
+    $db = pg_connect( "$host $port $dbname $credentials" );
+    if(!$db) {
+    echo "Error : Unable to open database\n";
+    } else {
+    echo "Opened database successfully\n";
+    }
 
-   $date = new DateTime();
-   $ts = $date->getTimestamp();
+    // id, eventName, firstName, lastName, fullName, location, comment, timeStamp, imageUpload
 
+    $today = date("Y-m-d");
+    $time = date("h:i:s");
+    $ts = $today . " " . $time;
   
-$sql = 'INSERT INTO public."commentFeed" ("eventName", "comment", "timeStamp") VALUES
-    (\'2I79C1\', \'This is a comment\', \'' . $ts . '\')';
-$result = pg_query($db, $sql);
-if(!$result){
-  echo pg_last_error($db);
-} else {
-  echo "Inserted successfully";
-}
+    $sql = 'INSERT INTO public."commentFeed" ("eventName", "comment", "timeStamp") VALUES
+        (\'2I79C1\', \'This is a comment\', \'' . $ts . '\')';
+    $result = pg_query($db, $sql);
+
+    if(!$result){
+      echo pg_last_error($db);
+    } else {
+      echo "Inserted successfully";
+    }
 
     pg_close($db);
 ?>
